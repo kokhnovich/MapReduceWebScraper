49ab5b32e47697cf249cb2ff6e87f54ec5498ea6
kokhnovich <akokhnovich@yandex.ru>
2020-05-19 00:47:14 +0300

    update

--- MapReduceStep.sh
+++ MapReduceStep.sh
@@ -2,7 +2,7 @@
 # "$1" has input file
 # "$2" has output file
 
-./scraper.py "$1" temp.txt
+./scraper.py <"$1" >temp.txt
 ./ttb temp.txt temp.bin
 ./sort temp.bin temp_sorted.bin
 ./btt temp_sorted.bin "$2"
--- scraper.py
+++ scraper.py
@@ -3,25 +3,19 @@ from requests import get
 from bs4 import BeautifulSoup
 import sys
 
-assert (len(sys.argv) == 3)
-input_file = sys.argv[1]
-output_file = sys.argv[2]
-
 
 def is_correct(s):
     return len(s) > 8 and (s[:8] == 'https://' or s[:8] == 'http://')
 
 
 visited = set()
-with open(output_file, 'w') as outp:
-    with open(input_file, 'r') as inp:
-        for line in inp:
-            if not is_correct(line): continue
-            req = get(line[:-1])
-            parser = BeautifulSoup(req.text, 'lxml')
+for line in sys.stdin:
+    if not is_correct(line): continue
+    req = get(line[:-1])
+    parser = BeautifulSoup(req.text, 'lxml')
 
-            for link in parser.find_all('a', href=True):
-                # @TODO add handlers for links started with '//' and '#'
-                ref = link['href']
-                if len(ref) < 60 and is_correct(ref):
-                    outp.write(ref + '\n')
+    for link in parser.find_all('a', href=True):
+        # @TODO add handlers for links started with '//' and '#'
+        ref = link['href']
+        if len(ref) < 60 and is_correct(ref):
+            print(ref)
