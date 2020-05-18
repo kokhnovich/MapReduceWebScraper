04189d9d6c37a83964994303ac0093ed6fcf374e
kokhnovich <akokhnovich@yandex.ru>
2020-05-19 01:50:16 +0300

    update scraper

--- scraper.py
+++ scraper.py
@@ -3,19 +3,24 @@ from requests import get
 from bs4 import BeautifulSoup
 import sys
 
-
 def is_correct(s):
-    return len(s) > 8 and (s[:8] == 'https://' or s[:8] == 'http://')
+    return len(s) > 8 and (s[:8] == 'https://' or s[:7] == 'http://')
 
 
 visited = set()
 for line in sys.stdin:
     if not is_correct(line): continue
-    req = get(line[:-1])
-    parser = BeautifulSoup(req.text, 'lxml')
+    try:
+        if line[:-1] in visited: continue
+        req = get(line[:-1])
+        parser = BeautifulSoup(req.text, 'lxml')
 
-    for link in parser.find_all('a', href=True):
-        # @TODO add handlers for links started with '//' and '#'
-        ref = link['href']
-        if len(ref) < 60 and is_correct(ref):
-            print(ref)
+        for link in parser.find_all('a', href=True):
+            if link in visited: continue
+            visited.add(link)
+            # @TODO add handlers for links started with '//' and '#'
+            ref = link['href']
+            if len(ref) < 60 and is_correct(ref):
+                print(ref)
+    except:
+        print("can't get '" + line[:-1] + "'", file=sys.stderr)
