ffcb42323afabeb7e8a9f119aac50c30c78b2ef6
kokhnovich <akokhnovich@yandex.ru>
2020-05-18 22:11:40 +0300

    update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from requests import get
 from bs4 import BeautifulSoup
 import sys
@@ -5,17 +6,25 @@ assert(len(sys.argv) == 3)
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 visited = set()
+
+def IsCorrect(s):
+  return len(s) > 8 and (s[:8] == 'https://' or s[:8] == 'http://')
+
 with open(output_file, 'w') as outp:
   with open(input_file, 'r') as inp:
     for line in inp:
-      # print("'"+line[:-1]+"'")
+      print("'"+line[:-1]+"'")
+      if not IsCorrect(line): continue
       req = get(line[:-1])
       parser = BeautifulSoup(req.text, 'lxml')
       
       for link in parser.find_all('a', href=True):
         ref = link['href'] 
         if ref[0] == '#' or len(ref) <= 2: continue
-        if ref[:2] == '//':
-          outp.write("https:" + link['href']+'\n')
-        else:
+        # if ref[:2] == '//':
+        #  ref = "https:" + link['href']
+        #  if len(ref) < 60:
+        #    outp.write(ref+'\n')
+        #else:
+        if len(ref) < 60 and IsCorrect(ref):
           outp.write(ref+'\n')
