c05870ad24b4b32b13227b8829fd8b1c3a6c0156
kokhnovich <akokhnovich@yandex.ru>
2020-05-20 15:40:32 +0300

    it works

--- ExtUnique.cpp
+++ ExtUnique.cpp
@@ -1,33 +1,14 @@
 #include <iostream>
-#include <fstream>
-#include <cassert>
 #include <string>
 
-// @TODO format to Key\tValue
-
 int main(int argc, char** argv) {
-  if (argc != 3) {
-    std::cerr << "Requires two arguments: source text file and destination binary file name" << std::endl;
-    return 1;
-  }
-
-  std::string input_file = argv[1];
-  std::string output_file = argv[2];
-
-  std::ifstream fin(input_file);
-  std::ofstream fout(output_file);
-
-  long long n;
-  fin >> n;
-  fin.ignore();
-  fout << n << std::endl;
-  std::string prev_line;
-  for (int i = 0; i < n; ++i) {
-    std::string line;
-    getline(fin, line);
-    if (i == 0 || line != prev_line) {
-      fout << line << std::endl;
-      prev_line = line;
+  std::string prev_key, now_key, now_value;
+  while (getline(std::cin, now_key, '\t')) {
+    getline(std::cin, now_value, '\n');
+    if (now_key != prev_key) {
+      std::cout << now_key << '\t' << now_value << std::endl;
+      prev_key = now_key;
     }
   }
+  std::cout.flush();
 }
--- MapReduceMain.cpp
+++ MapReduceMain.cpp
@@ -56,6 +56,7 @@ void Map(const MainInfo& info) {
   std::vector<bp::child> child_processes;
   std::string list_of_all_output_files;
 
+  std::cout << info.input_path << std::endl;
   std::ifstream in(info.input_path);
   std::vector<std::string> local_input;
   local_input.reserve(LENGTH_TO_DIVIDE_MAP);
@@ -80,7 +81,9 @@ void Map(const MainInfo& info) {
   for (auto& child : child_processes) {
     child.wait();
   }
+  assert(!child_processes.empty());
   std::cout << child_processes.size() << std::endl;
+  std::cout << list_of_all_output_files << std::endl;
   int code = bp::system("cat " + list_of_all_output_files, bp::std_out > info.output_path, bp::std_err > stderr);
   if (code != 0) {
     std::cerr << "error while cat files" << std::endl;
@@ -88,61 +91,22 @@ void Map(const MainInfo& info) {
   }
   RemoveFiles(input_files);
   RemoveFiles(output_files);
+  exit(0);
 }
 
 void Reduce(const MainInfo& info) {
-  std::vector<std::pair<std::string, int>> words;
-  { // input data
-    std::string word;
-    int cnt;
-    std::ifstream stream(info.input_path);
-    while (stream >> word >> cnt) {
-      words.emplace_back(word, cnt);
-    }
+  if (bp::system("bash SortTxtFile.sh " + info.input_path) != 0) {
+    std::cerr << "error while sort" << std::endl;
+    return;
   }
-
-  std::sort(words.begin(), words.end());
-
-  std::vector<std::string> input_files;
-  std::vector<std::string> output_files;
-  std::vector<bp::child> child_processes;
-  for (int i = 0; i < words.size();) {
-    std::string input_file = GetFileName("input", i);
-    std::string output_file = GetFileName("output", i);
-
-    input_files.push_back(input_file);
-    output_files.push_back(output_file);
-
-    std::ofstream os(input_file);
-    int first_index = i;
-    while (i < words.size() && words[i].first == words[first_index].first) {
-      os << words[i].first << ' ' << words[i].second << std::endl;
-      ++i;
-    }
-    child_processes.emplace_back(info.script_path,
-                                 bp::std_out > output_file,
-                                 bp::std_err > stderr,
-                                 bp::std_in < input_file);
+  int code = bp::system(info.script_path,
+                        bp::std_out > info.output_path,
+                        bp::std_err > stderr,
+                        bp::std_in < info.input_path);
+  if (code != 0) {
+    std::cerr << "error while reduce" << std::endl;
+    exit(code);
   }
-
-  for (auto& child : child_processes) {
-    child.wait();
-  }
-
-  RemoveFiles(input_files);
-
-  { // output data
-    std::ofstream stream(info.output_path);
-    for (const auto& name : output_files) {
-      std::ifstream in(name);
-      std::string word;
-      int count;
-      while (in >> word >> count) {
-        stream << word << ' ' << count << std::endl;
-      }
-    }
-  }
-  RemoveFiles(output_files);
 };
 
 int main(int argc, char* argv[]) {
@@ -151,14 +115,9 @@ int main(int argc, char* argv[]) {
     return 0;
   }
   MainInfo main_info{argv[1], argv[2], argv[3], argv[4]};
-  std::string mode = argv[1];
-  std::string script_path = argv[2];
-  std::string input_path = argv[3];
-  std::string output_path = argv[4];
-
-  if (mode == "map") {
+  if (main_info.mode == "map") {
     Map(main_info);
-  } else if (mode == "reduce") {
+  } else if (main_info.mode == "reduce") {
     Reduce(main_info);
   } else {
     std::cerr << R"(mode can be only "map" or "reduce")" << std::endl;
