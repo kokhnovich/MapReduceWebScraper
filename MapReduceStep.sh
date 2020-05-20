fefc6edb3a5481700f15b0d9e71a1a2d334afbd3
kokhnovich <akokhnovich@yandex.ru>
2020-05-20 20:45:39 +0300

    change MR algo

--- MapReduceStep.sh
+++
@@ -1,9 +0,0 @@
-
-# "$1" has input file
-# "$2" has output file
-
-./scraper.py <"$1" >temp.txt
-./ttb temp.txt temp.bin
-./sort temp.bin temp_sorted.bin
-./btt temp_sorted.bin "$2"
-rm temp.txt temp.bin temp_sorted.bin
