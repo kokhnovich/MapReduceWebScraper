04a5c3d1bc6b1178b3950fbebbc01f48231eddbb
kokhnovich <akokhnovich@yandex.ru>
2020-05-22 02:55:32 +0300

    update buildscript

--- BuildScripts.sh
+++ BuildScripts.sh
@@ -1,5 +1,1 @@
-g++ ExtSort.cpp -o sort -std=c++17
-g++ TxtToBin.cpp -o ttb -std=c++17
-g++ BinToTxt.cpp -o btt -std=c++17
 g++ MapReduceMain.cpp -o mr -pthread -std=c++17
-g++ ExtUnique.cpp -o uniq
