a76d713ab757f21067bcb622a7017f8eb5d58c2e
kokhnovich <akokhnovich@yandex.ru>
2020-05-22 04:09:16 +0300

    fixes

--- WebScraperMainScript.sh
+++ WebScraperMainScript.sh
@@ -1,7 +1,6 @@
-# $1 - depth
-depth="$1"
+depth="$3"
 
-cat input.txt > last_output.txt
+cat "$1" > last_output.txt
 while [[ "$depth" -gt 0 ]]; do
   echo "running map..." &&
   time ./mr map ./Scraper.py last_output.txt temp.txt &&
@@ -10,10 +9,9 @@ while [[ "$depth" -gt 0 ]]; do
   time ./mr reduce ./reducer temp.txt last_output.txt &&
 
   echo "checking..." &&
-
   wc -l last_output.txt &&
   rm temp.txt &&
   ((depth--))
 done
-cat last_output.txt > output.txt
+cat last_output.txt > "$2"
 rm last_output.txt
